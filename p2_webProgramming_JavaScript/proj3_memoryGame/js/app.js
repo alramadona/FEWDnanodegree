3dcf053f410ca71641b405e75be37654c66cc45f
Aditya Lia Ramadona <alramadona@outlook.com>
2018-03-18 16:43:46 +0200

    3rd project back to basic

--- p2_webProgramming_JavaScript/proj3_memoryGame/js/app.js
+++ p2_webProgramming_JavaScript/proj3_memoryGame/js/app.js
@@ -26,7 +26,7 @@ function shuffle(array) {
 }
 
 
-/**
+/*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -35,4 +35,4 @@ function shuffle(array) {
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- **/
+ */
